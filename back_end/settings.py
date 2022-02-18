364ffc5b89830922b78c6147c018edfbb917470e
Alexei <hlbu@yandex.ru>
2022-02-18 11:33:02 +0300

    del s

--- back_end/settings.py
+++ back_end/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # others:
+    # other:
     'rest_framework',
     # my apps:
     'mainapp',
